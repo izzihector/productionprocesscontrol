397147db329ea5ade34ca3ce29773ddd36c07fed
Hector Cerezo <booter@MacBook-Air-de-Hector.local>
2020-03-24 22:37:34 +0100

    Project revenue start

---
+++ project-revenue/models/project_revenue.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class project_revenue(models.Model):
+    _name = 'project_revenue.project_revenue'
+    _description = 'project_revenue'
+    _inherit = 'project.project'
+
+    name = fields.Char()
