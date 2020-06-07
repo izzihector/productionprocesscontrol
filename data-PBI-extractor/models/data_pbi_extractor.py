dcc8a7642724b4efec53a31d6b7c886c0ffc6470
Héctor <cetohector@gmail.com>
2020-06-07 17:51:41 +0200

    Success float change

--- data-PBI-extractor/models/data_pbi_extractor.py
+++ data-PBI-extractor/models/data_pbi_extractor.py
@@ -23,6 +23,8 @@ class DataPbiExtractor(models.Model):
 
     @api.multi
     def get_informe_horas_vendidas_imputadas_analityc(self):
+        #Controlar que es una devolucion: refund_invoice_id controla si tiene cantidades dvueltas (account.invoice)
+        #Controlar que viene la fra de un pedido: origin (account.invoice)
         now = datetime.now()  # current date and time
 
         date_time = now.strftime("%m_%d_%Y_%H_%M_%S")
@@ -58,6 +60,7 @@ class DataPbiExtractor(models.Model):
                 responsable = project.user_id.name
                 unidades_vendidas = 0
                 proyectoCerrado = "NO"
+                order_name = ""
 
                 # Primero visitamos las tareas para obtener las horas imputadas y si estas tareas
                 # tienen lineas de venta, las recogemos con el fin de ir acumulando
@@ -75,13 +78,19 @@ class DataPbiExtractor(models.Model):
                             for sale_line in sales_lines:
                                 if sale_line['id'] != idLineaPedido:
                                     total_quantity_line = sale_line['product_uom_qty']
-                                    total_quantity_for_project = total_quantity_for_project + total_quantity_line
-                                    if sale_line.horas_reales > 0:
-                                        is_closed_project = 1
-                                        proyectoCerrado = "SI"
-                                        horas_proyecto_cerrado = horas_proyecto_cerrado + sale_line.horas_reales
+                                    #Comprobamos si el pedido de esta linea tiene factura, si la tiene
+                                    #comprobamos que no tiene refounds, si los tiene, no sumamos la cantidad de horas
+                                    #vendidas
+                                    order_name = sale_line['order_id'].name
+                                    order_state = sale_line['order_id'].state
+                                    if self.descartar_facturas_devolucion(order_name) == 0 and self.check_order_is_active(order_state) == 1:
+                                        total_quantity_for_project = total_quantity_for_project + total_quantity_line
+                                        if sale_line.horas_reales > 0:
+                                            is_closed_project = 1
+                                            proyectoCerrado = "SI"
+                                            horas_proyecto_cerrado = horas_proyecto_cerrado + sale_line.horas_reales
 
-                                    idLineaPedido = sale_line['id']
+                                        idLineaPedido = sale_line['id']
                         i = i + 1
 
                 if project_id:
@@ -107,10 +116,15 @@ class DataPbiExtractor(models.Model):
                 totalHorasContratadas = str(total_horas_contratadas)
                 totalHorasContratadas = totalHorasContratadas.replace('.', ',')
 
+
+
                 total_horas_imputadas = total_worked_hours
 
                 totalHorasImputadas = str(total_horas_imputadas)
                 totalHorasImputadas = totalHorasImputadas.replace('.', ',')
+                totalHorasImputadas = float(totalHorasImputadas)
+                totalHorasImputadas = '{0:02.0f}:{1:02.0f}'.format(*divmod(totalHorasImputadas * 60, 60))
+
 
                 if total_quantity_for_project == 0:
                     alert_percentil_no_profitable = 1000
@@ -131,6 +145,42 @@ class DataPbiExtractor(models.Model):
         return self.write(
             {'file_name_project_horas_vendidas_vs_realizadas': filename,
              'file_binary_project_horas_vendidas_vs_realizadas': content, 'name': filename, 'model': 'PBI: Proyectos'})
+
+
+    def check_order_is_active(self, estado_pedido):
+        if estado_pedido == "cancel":
+            return 0
+
+        return 1
+
+    def descartar_facturas_devolucion(self, nombre_pedido_venta):
+        AI = self.env['account.invoice']
+
+        facturas = AI.search([('origin','=', nombre_pedido_venta)])
+
+        if facturas:
+            for factura in facturas:
+                facturas_devueltas = factura['refund_invoice_ids']
+                if facturas_devueltas:
+                    #Tiene devolucion, no lo contabilizamos
+                    return 1
+                else:
+                    return 0
+
+        return 0
+
+    def _get_name_tipo_proyecto(self, idproyecto):
+        resultado = "Sin definir"
+        if idproyecto == "1":
+            resultado = "Mantenimiento"
+        if idproyecto == "2":
+            resultado = "Pack horas"
+        if idproyecto == "3":
+            resultado = "Proyecto cerrado"
+        if idproyecto == "4":
+            resultado = "Horas facturables"
+
+        return resultado
 
     @api.multi
     def get_tickets_analityc(self):
@@ -192,16 +242,3 @@ class DataPbiExtractor(models.Model):
 
         return self.write(
             {'file_name': filename, 'file_binary': content, 'name': filename, 'model': 'PBI: Tickets'})
-
-    def _get_name_tipo_proyecto(self,idproyecto):
-        resultado = "Sin definir"
-        if idproyecto == "1":
-            resultado = "Mantenimiento"
-        if idproyecto == "2":
-            resultado = "Pack horas"
-        if idproyecto == "3":
-            resultado = "Proyecto cerrado"
-        if idproyecto == "4":
-            resultado = "Horas facturables"
-            
-        return resultado
