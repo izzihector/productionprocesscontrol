5717ae9d872b40683ec9e3f590af233f43529b90
Héctor <cetohector@gmail.com>
2020-06-22 10:28:26 +0200

    Arreglamos la obtención de horas efectivas

--- data-PBI-extractor/models/data_pbi_extractor.py
+++ data-PBI-extractor/models/data_pbi_extractor.py
@@ -23,8 +23,8 @@ class DataPbiExtractor(models.Model):
 
     @api.multi
     def get_informe_horas_vendidas_imputadas_analityc(self):
-        #Controlar que es una devolucion: refund_invoice_id controla si tiene cantidades dvueltas (account.invoice)
-        #Controlar que viene la fra de un pedido: origin (account.invoice)
+        # Controlar que es una devolucion: refund_invoice_id controla si tiene cantidades dvueltas (account.invoice)
+        # Controlar que viene la fra de un pedido: origin (account.invoice)
         now = datetime.now()  # current date and time
 
         date_time = now.strftime("%m_%d_%Y_%H_%M_%S")
@@ -37,9 +37,11 @@ class DataPbiExtractor(models.Model):
             # Proyecto y Tarea
             writer.writerow(
                 ['Id proyecto', 'Proyecto', 'Cliente', 'Codigo Cliente', 'Tipo Proyecto', 'Responsable',
-                 'Horas vendidas', 'Horas imputadas', '% Alerta', 'Proyecto Cerrado', 'Departamento', 'Comercial', 'Etapa'])
+                 'Horas vendidas', 'Horas imputadas', '% Alerta', 'Proyecto Cerrado', 'Departamento', 'Comercial',
+                 'Etapa'])
 
             PP = self.env['project.project']
+            PT = self.env['project.task']
             SSL = self.env['sale.subscription.line']
             AIL = self.env['account.invoice.line']
 
@@ -50,7 +52,7 @@ class DataPbiExtractor(models.Model):
                 is_closed_project = 0
                 total_quantity_for_project = 0
                 total_worked_hours = 0
-                tasks = project.task_ids
+                # tasks = project.task_ids
                 project_id = project.id
                 project_name = project.name
                 alert_percentil_no_profitable = 0
@@ -68,6 +70,8 @@ class DataPbiExtractor(models.Model):
                 # Primero visitamos las tareas para obtener las horas imputadas y si estas tareas
                 # tienen lineas de venta, las recogemos con el fin de ir acumulando
                 # las horas vendidas
+                tasks = PT.search([('project_id', '=', project_id)])
+
                 if tasks:
                     i = 0
                     idLineaPedido = ""
@@ -81,12 +85,13 @@ class DataPbiExtractor(models.Model):
                             for sale_line in sales_lines:
                                 if sale_line['id'] != idLineaPedido:
                                     total_quantity_line = sale_line['product_uom_qty']
-                                    #Comprobamos si el pedido de esta linea tiene factura, si la tiene
-                                    #comprobamos que no tiene refounds, si los tiene, no sumamos la cantidad de horas
-                                    #vendidas
+                                    # Comprobamos si el pedido de esta linea tiene factura, si la tiene
+                                    # comprobamos que no tiene refounds, si los tiene, no sumamos la cantidad de horas
+                                    # vendidas
                                     order_name = sale_line['order_id'].name
                                     order_state = sale_line['order_id'].state
-                                    if self.descartar_facturas_devolucion(order_name) == 0 and self.check_order_is_active(order_state) == 1:
+                                    if self.descartar_facturas_devolucion(
+                                            order_name) == 0 and self.check_order_is_active(order_state) == 1:
                                         total_quantity_for_project = total_quantity_for_project + total_quantity_line
                                         if sale_line.horas_reales > 0:
                                             is_closed_project = 1
@@ -120,6 +125,7 @@ class DataPbiExtractor(models.Model):
                 totalHorasContratadas = totalHorasContratadas.replace('.', ',')
 
                 total_horas_imputadas = total_worked_hours
+                # totalHorasImputadas = total_worked_hours
 
                 totalHorasImputadas = str(total_horas_imputadas)
                 totalHorasImputadas = totalHorasImputadas.replace('.', ',')
@@ -144,7 +150,6 @@ class DataPbiExtractor(models.Model):
             {'file_name_project_horas_vendidas_vs_realizadas': filename,
              'file_binary_project_horas_vendidas_vs_realizadas': content, 'name': filename, 'model': 'PBI: Proyectos'})
 
-
     def check_order_is_active(self, estado_pedido):
         if estado_pedido == "cancel":
             return 0
@@ -154,13 +159,13 @@ class DataPbiExtractor(models.Model):
     def descartar_facturas_devolucion(self, nombre_pedido_venta):
         AI = self.env['account.invoice']
 
-        facturas = AI.search([('origin','=', nombre_pedido_venta)])
+        facturas = AI.search([('origin', '=', nombre_pedido_venta)])
 
         if facturas:
             for factura in facturas:
                 facturas_devueltas = factura['refund_invoice_ids']
                 if facturas_devueltas:
-                    #Tiene devolucion, no lo contabilizamos
+                    # Tiene devolucion, no lo contabilizamos
                     return 1
                 else:
                     return 0
