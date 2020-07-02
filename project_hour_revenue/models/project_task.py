9e53addf88edbc48031d466847d53d679e307ee9
Héctor <cetohector@gmail.com>
2020-07-02 13:07:05 +0200

    fix bug total

--- project_hour_revenue/models/project_task.py
+++ project_hour_revenue/models/project_task.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from datetime import datetime, timedelta
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models, _, exceptions
 from odoo.exceptions import ValidationError
+import math
 
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    horas_restantes_produccion_proyecto = fields.Float(
+    horas_restantes_produccion_proyecto = fields.Char(
         string='Total Horas Restantes Produccion',
         required=False, compute='_calc_hours_less_for_proyect')
 
@@ -47,14 +49,38 @@ class ProjectTask(models.Model):
                             for sale_line in sales_lines:
                                 have_project = sale_line['x_studio_proyecto_pedido_venta']
                                 total_quantity_line = sale_line['product_uom_qty']
+                                horas_reales = sale_line['horas_reales']
+                                if horas_reales:
+                                    total_quantity_line = horas_reales
                                 if have_project:
                                     total_quantity_line = sale_line['horas_reales']
+
                                 total_quantity_for_project = total_quantity_for_project + total_quantity_line
 
-                        horas_restantes_produccion = total_quantity_for_project - total_worked_hours
-            self.horas_restantes_produccion_proyecto = self.convert_time_unit_to_hours(horas_restantes_produccion)
+                horas_restantes_produccion = total_quantity_for_project - total_worked_hours
+
+                is_negative = 0
+                negative_code = "-"
+
+                if (negative_code in str(horas_restantes_produccion)):
+                    is_negative = 1
+
+                # if horas_restantes_produccion > 1:
+                # horas = float(horas_restantes_produccion)
+
+                seconds = abs(horas_restantes_produccion) * 60 * 60
+                minutes, seconds = divmod(seconds, 60)
+                hours, minutes = divmod(minutes, 60)
+
+                horas_restantes_produccion = "%02d:%02d" % (hours, minutes)
+
+                if is_negative == 1:
+                    horas_restantes_produccion = "-" + horas_restantes_produccion
+
+            self.horas_restantes_produccion_proyecto = horas_restantes_produccion
 
     def convert_time_unit_to_hours(self, time_unit):
+
         total_time_hour = 100
         total_minutes_hour = 60
         es_negativo = 0
