38fefe6958e1586d168088d7be96e2978366c881
Hector <info@hootei.es>
2020-07-06 12:04:39 +0200

    All upload

--- virtual_model_to_view/models/virtual_model_to_view.py
+++ virtual_model_to_view/models/virtual_model_to_view.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, fields, api
+from odoo import models, fields, api, tools
 import base64
 import csv
 from datetime import datetime
@@ -10,6 +10,17 @@ _logger = logging.getLogger(__name__)
 
 
 class VirtualModelToView(models.Model):
-    _name = "virtual_model_to_view"
+    _name = "virtual.model.to.view"
+    _auto = "false"
 
-    name = fields.Char(string='Nombre')
+    project_name = fields.Char(string='Nombre')
+
+    @api.model_cr
+    def init(self):
+        tools.drop_view_if_exists(self._cr, 'project_milestone_invoicing')
+        self._cr.execute("""
+                CREATE OR REPLACE VIEW project_virtual AS (
+                    SELECT 
+                    ps.display_name as project_name
+                    FROM public.project_project ps
+                )""")
