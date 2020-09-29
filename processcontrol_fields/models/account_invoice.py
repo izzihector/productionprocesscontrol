4ee30ea0e9d6c7aba577aee55d326ba7ac88f27b
Hector <info@hootei.es>
2020-09-29 09:18:21 +0200

    Test

--- processcontrol_fields/models/account_invoice.py
+++ processcontrol_fields/models/account_invoice.py
@@ -14,6 +14,41 @@ class AccountInvoice(models.Model):
 
     @api.model
     def create(self, vals):
+
+        origen = vals['origin']
+        SO = self.env['sale.order']
+        SOL = self.env['sale.order.line']
+        es_servicio = 0
+        mensaje = ""
+
+        if origen:
+            if origen != "":
+                pedido = SO.search([('name', '=', origen)])
+                if pedido:
+                    pedido_id = pedido.id
+                    lineas_pedido = SOL.search([('order_id', '=', pedido_id)])
+                    if lineas_pedido:
+                        for linea in lineas_pedido:
+                            producto = linea.product_id
+                            if producto:
+                                categoria_es_servicio_proyecto_cerrado = producto.categ_id.category_for_project_closed
+                                if categoria_es_servicio_proyecto_cerrado:
+                                    es_servicio = 1
+                                    if linea.x_studio_proyecto_pedido_venta.id == "" or linea.x_studio_proyecto_pedido_venta.id == False:
+                                        # Si entra en esta condicion, no tiene proyecto indicado pero si que lo necesita
+                                        mensaje = mensaje + "\n En la linea del producto: " + producto.name + " es obligado establecer un proyecto."
+                                        if linea.product_uom_qty == 1:
+                                            if linea.horas_reales == 0:
+                                                mensaje = mensaje + "\n La linea del producto: " + producto.name + " no puede tener 0 horas reales."
+
+                                    if linea.x_studio_proyecto_pedido_venta.id:
+                                        if linea.product_uom_qty == 1:
+                                            if linea.horas_reales == 0:
+                                                mensaje = mensaje + "\n La linea del producto: " + producto.name + " no puede tener 0 horas reales."
+
+        if es_servicio == 1 and mensaje != "":
+            raise ValidationError(_(mensaje))
+
         str = "SUB"
         origen = vals['origin']
         isProyectoCerrado = 0
