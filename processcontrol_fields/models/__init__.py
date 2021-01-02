9f1b99c835933fa270751e43fccc59e476ffbb77
Héctor <cetohector@gmail.com>
2021-01-02 19:47:59 +0100

    Update __init__.py
    Quitamos sale suscription

--- processcontrol_fields/models/__init__.py
+++ processcontrol_fields/models/__init__.py
@@ -4,6 +4,5 @@ from . import helpdesk_ticket
 from . import sale_order_line
 from . import account_invoice
 from . import account_invoice_line
-from . import sale_subscription
 from . import product_category
 from . import project_task
