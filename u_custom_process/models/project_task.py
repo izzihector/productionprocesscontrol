b55c863af72f05b9b3c068e89fb790d2c7c414e4
José Angel Inda Herrera <jose.inda09947@gmail.com>
2021-04-20 22:50:31 -0400

    Restringir crear tareas y horas en proyectos archivados

---
+++ u_custom_process/models/project_task.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from datetime import datetime, timedelta
+
+from odoo import api, fields, models, _, exceptions
+from odoo.exceptions import ValidationError
+import math
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    @api.model
+    def create(self, vals):
+        res = super(ProjectTask, self).create(vals)
+        project_id = vals.get("project_id")
+        if project_id:
+            project = self.env['project.project'].browse(project_id)
+            if not project.active:
+                raise ValidationError(_("Can't task create to Archived project."))            
+        return res
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    @api.model
+    def create(self, vals):
+        res = super(AccountAnalyticLine, self).create(vals)
+        project_id = vals.get("project_id")
+        if project_id:
+            project = self.env['project.project'].browse(project_id)
+            if not project.active:
+                raise ValidationError(_("Can't task create to Archived project."))            
+        return res
