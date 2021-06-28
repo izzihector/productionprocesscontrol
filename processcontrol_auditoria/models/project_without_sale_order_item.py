b326d53b00eef57c79e1d526622b2d03895b58a2
carolinafernandez-processcontrol <84458198+carolinafernandez-processcontrol@users.noreply.github.com>
2021-06-28 08:14:40 -0300

    auditoria
    auditoria

---
+++ processcontrol_auditoria/models/project_without_sale_order_item.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from xlwt import *
+from io import BytesIO
+import base64
+import pdb
+
+
+class project_without_sale_order_item(models.TransientModel):
+    _name = 'project.without.sale.order.item'
+    _description = 'Project Without Sale Order Item'
+
+    name = fields.Char(string='Proyecto sin Pedido de Venta')
+    type_report = fields.Selection([
+        ('all', 'Todo'),
+        ('project', 'Proyectos'),
+        ('task', 'Tareas'),
+        ('time_sheet', 'Parte de horas')], string='Tipo de Informe', required=True, default='all')
+    exclude_project = fields.Boolean(string='Excluir Proyecto')
+    description = fields.Text(
+        default='Reporte que según el tipo de informe que se seleccione realiza diferentes operaciones sobre los Proyectos, \n'
+                'Tareas y/o Parte de horas, si seleccionamos excluir proyectos y agregamos proyectos, estos se filtraran en el reporte\n'
+                'ya sea en ellos mismos o en las Tareas y Partes de Hora que pertenezcan.\n'
+                'Tipo de Informe: \n'
+                '               -Proyectos: Devuelve todos los proyectos que no tengan un Pedido de Venta \n'
+                '               -Tareas: Devuelve todas las tareas que no tengan un Pedido de Venta \n'
+                '               -Parte de horas: Devuelve todas las parte de horas que no tengan un Pedido de Venta \n'
+                '               -Todo: Devuelve las 3 opciones anteriores en un solo reporte, sepradas en hojas \n',
+        string='Descripción:', readonly=True)
+    projects_ids = fields.Many2many('project.project', string='Proyectos')
+
+    file = fields.Binary(string="Reporte")
+
+    # Project
+    @staticmethod
+    def project_without_sale_order_item_excel(wb, project_info_list):
+        row = 0
+        title = easyxf('font: name Calibri, bold True; alignment: horizontal left')
+        lines = easyxf('font: name Calibri; alignment: horizontal left')
+        lines_error = easyxf('font: name Calibri; alignment: horizontal left; pattern: pattern solid,'
+                             ' fore_colour light_yellow;')
+        ws = wb.add_sheet('Proyectos sin Pedido de Ventas', cell_overwrite_ok=True)
+        ws.write(row, 0, "Nombre del proyecto", title)
+        ws.write(row, 1, "Cliente", title)
+        ws.write(row, 2, "Responsable del proyecto", title)
+        ws.write(row, 3, "Tipo de proyecto", title)
+        for info in project_info_list:
+            row += 1
+            ws.write(row, 0, info[0], lines)
+            if info[1] != 'No tiene':
+                ws.write(row, 1, info[1], lines)
+            else:
+                ws.write(row, 1, info[1], lines_error)
+            if info[2] != 'No tiene':
+                ws.write(row, 2, info[2], lines)
+            else:
+                ws.write(row, 2, info[2], lines_error)
+            if info[3] != 'No tiene':
+                ws.write(row, 3, info[3], lines)
+            else:
+                ws.write(row, 3, info[3], lines_error)
+
+        ws.col(0).width += 4000
+        ws.col(1).width += 3000
+        ws.col(2).width += 4000
+        ws.col(3).width += 3000
+
+    def get_project_without_sale_order_item(self, wb):
+        project_obj = self.env['project.project']
+        if self.exclude_project:
+            projects = project_obj.search([('sale_line_id', '=', False), ('id', 'not in', self.projects_ids.ids)])
+        else:
+            projects = project_obj.search([('sale_line_id', '=', False)])
+        if self.type_report == 'project' and not projects:
+            raise UserError(_('Dont found any Project without Sales Order Item'))
+        project_info_list = []
+        for pro in projects:
+            info = []
+            info.append(pro.name)
+            info.append(pro.partner_id.name if pro.partner_id else 'No tiene')
+            info.append(pro.user_id.name if pro.user_id else 'No tiene')
+            info.append(dict(pro._fields['type_project'].selection).get(pro.type_project) if pro.type_project else 'No tiene')
+            project_info_list.append(info)
+        if project_info_list:
+            self.project_without_sale_order_item_excel(wb, project_info_list)
+
+        return project_info_list
+
+    # Task
+    @staticmethod
+    def task_without_sale_order_item_excel(wb, task_info_list):
+        row = 0
+        title = easyxf('font: name Calibri, bold True; alignment: horizontal left')
+        lines = easyxf('font: name Calibri; alignment: horizontal left')
+        lines_error = easyxf('font: name Calibri; alignment: horizontal left; pattern: pattern solid,'
+                             ' fore_colour light_yellow;')
+        ws = wb.add_sheet('Tareas sin Pedido de Ventas', cell_overwrite_ok=True)
+        ws.write(row, 0, "Nombre del proyecto", title)
+        ws.write(row, 1, "Cliente", title)
+        ws.write(row, 2, "Pedido de venta del proyecto", title)
+        ws.write(row, 3, "Nombre tarea", title)
+        ws.write(row, 4, "Asignado a", title)
+        for info in task_info_list:
+            row += 1
+            ws.write(row, 0, info[0], lines)
+            if info[1] != 'No tiene':
+                ws.write(row, 1, info[1], lines)
+            else:
+                ws.write(row, 1, info[1], lines_error)
+            if info[2] != 'No tiene':
+                ws.write(row, 2, info[2], lines)
+            else:
+                ws.write(row, 2, info[2], lines_error)
+            ws.write(row, 3, info[3], lines)
+            if info[4] != 'No tiene':
+                ws.write(row, 4, info[4], lines)
+            else:
+                ws.write(row, 4, info[4], lines_error)
+
+        ws.col(0).width += 4000
+        ws.col(1).width += 3000
+        ws.col(2).width += 5000
+        ws.col(3).width += 6000
+        ws.col(4).width += 3000
+
+    def get_task_without_sale_order_item(self, wb):
+        task_obj = self.env['project.task']
+        if self.exclude_project:
+            tasks = task_obj.search([('sale_line_id', '=', False), ('project_id', 'not in', self.projects_ids.ids)])
+        else:
+            tasks = task_obj.search([('sale_line_id', '=', False)])
+        if self.type_report == 'task' and not tasks:
+            raise UserError(_('Dont found any Tasks without Sales Order Item'))
+        task_info_list = []
+        for tas in tasks:
+            info = []
+            project = tas.project_id
+            info.append(project.name)
+            info.append(tas.partner_id.name if tas.partner_id else 'No tiene')
+            info.append(project.sale_line_id.name if project.sale_line_id else 'No tiene')
+            info.append(tas.name)
+            info.append(tas.user_id.name if tas.user_id else 'No tiene')
+            task_info_list.append(info)
+        if task_info_list:
+            self.task_without_sale_order_item_excel(wb, task_info_list)
+
+        return task_info_list
+
+    # Time Sheet
+    @staticmethod
+    def time_sheet_without_sale_order_item_excel(wb, time_sheet_info_list):
+        row = 0
+        title = easyxf('font: name Calibri, bold True; alignment: horizontal left')
+        lines = easyxf('font: name Calibri; alignment: horizontal left')
+        lines_error = easyxf('font: name Calibri; alignment: horizontal left; pattern: pattern solid,'
+                             ' fore_colour light_yellow;')
+        ws = wb.add_sheet('Parte de Horas sin Pedido de Ventas', cell_overwrite_ok=True)
+        ws.write(row, 0, "Fecha", title)
+        ws.write(row, 1, "Proyecto", title)
+        ws.write(row, 2, "Tarea", title)
+        ws.write(row, 3, "Tiquete del Servicio de Asistencia", title)
+        ws.write(row, 4, "Descripción", title)
+        ws.write(row, 5, "Cliente del ticket", title)
+        ws.write(row, 6, "Tipo facturable", title)
+        for info in time_sheet_info_list:
+            row += 1
+            ws.write(row, 0, info[0], lines)
+            if info[1] != 'No tiene':
+                ws.write(row, 1, info[1], lines)
+            else:
+                ws.write(row, 1, info[1], lines_error)
+            if info[2] != 'No tiene':
+                ws.write(row, 2, info[2], lines)
+            else:
+                ws.write(row, 2, info[2], lines_error)
+            if info[3] != 'No tiene':
+                ws.write(row, 3, info[3], lines)
+            else:
+                ws.write(row, 3, info[3], lines_error)
+            ws.write(row, 4, info[4], lines)
+            if info[5] != 'No tiene':
+                ws.write(row, 5, info[5], lines)
+            else:
+                ws.write(row, 5, info[5], lines_error)
+            if info[6] != 'No tiene':
+                ws.write(row, 6, info[6], lines)
+            else:
+                ws.write(row, 5, info[5], lines_error)
+
+        ws.col(0).width += 3000
+        ws.col(1).width += 4000
+        ws.col(2).width += 6000
+        ws.col(3).width += 8000
+        ws.col(4).width += 8000
+        ws.col(5).width += 3000
+        ws.col(6).width += 3000
+
+    def get_time_sheet_without_sale_order_item(self, wb):
+        aal_obj = self.env['account.analytic.line']
+        if self.exclude_project:
+            time_sheet = aal_obj.search([('so_line', '=', False), ('project_id', 'not in', self.projects_ids.ids)])
+        else:
+            time_sheet = aal_obj.search([('so_line', '=', False)])
+        if self.type_report == 'time_sheet' and not time_sheet:
+            raise UserError(_('Dont found any time_sheets without Sales Order Item'))
+        time_sheet_info_list = []
+        for ts in time_sheet:
+            info = []
+            info.append(ts.date.strftime("%d/%m/%Y"))
+            info.append(ts.project_id.name if ts.project_id else 'No tiene')
+            info.append(ts.task_id.name if ts.task_id else 'No tiene')
+            info.append(ts.helpdesk_ticket_id.name if ts.helpdesk_ticket_id else 'No tiene')
+            info.append(ts.name)
+            info.append(ts.x_cliente_del_ticket.name if ts.x_cliente_del_ticket else 'No tiene')
+            info.append(dict(ts._fields['timesheet_invoice_type'].selection).get(ts.timesheet_invoice_type) if ts.timesheet_invoice_type else 'No tiene')
+            time_sheet_info_list.append(info)
+        if time_sheet_info_list:
+            self.time_sheet_without_sale_order_item_excel(wb, time_sheet_info_list)
+
+        return time_sheet_info_list
+
+    # Button
+    @api.multi
+    def project_without_sale_order_item_report(self):
+        if self.exclude_project and not self.projects_ids:
+            raise UserError(_('Must selected a project'))
+        wb = Workbook(encoding='utf-8')
+        if self.type_report in ('all', 'project'):
+            file_name = 'Proyectos sin Pedido de Ventas.xls'
+            projects = self.get_project_without_sale_order_item(wb)
+        if self.type_report in ('all', 'task'):
+            file_name = 'Tareas sin Pedido de Ventas.xls'
+            tasks = self.get_task_without_sale_order_item(wb)
+        if self.type_report in ('all', 'time_sheet'):
+            file_name = 'Parte de horas sin Pedido de Ventas.xls'
+            time_sheet = self.get_time_sheet_without_sale_order_item(wb)
+        if self.type_report == 'all':
+            if not projects and not tasks and not time_sheet:
+                raise UserError(_('Dont found any Project, Tasks and Time Sheet without Sales Order Item'))
+            file_name = 'Todo sin Pedido de Ventas.xls'
+
+        fp = BytesIO()
+        wb.save(fp)
+        fp.seek(0)
+        data = fp.read()
+        fp.close()
+        data_to_save = base64.encodebytes(data)
+        wiz_id = self.env['descargar.hojas'].create({'archivo_nombre': file_name, 'archivo_contenido': data_to_save})
+        return {
+            'name': "Descargar Archivo",
+            'type': 'ir.actions.act_window',
+            'res_model': 'descargar.hojas',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': wiz_id.id,
+            'views': [(False, 'form')],
+            'target': 'new',
+        }
---
+++ processcontrol_auditoria/models/sale_order_subscription_report.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, _
+from odoo.exceptions import UserError
+from xlwt import *
+from io import BytesIO
+import base64
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
+class sale_order_subscription_report(models.TransientModel):
+    _name = 'sale.order.subscription.report'
+    _description = 'Sale Order Subscription Report'
+
+    def _get_default_start_date(self):
+        today = date.today()
+        return today.replace(day=1)
+
+    def _get_default_stop_date(self):
+        return date.today()
+
+    name = fields.Char(string='Pedido de venta de subscripcion')
+    start = fields.Date(string='Desde', required=True, default=_get_default_start_date)
+    stop = fields.Date(string='Hasta', required=True, default=_get_default_stop_date)
+    description = fields.Text(
+        default='Reporte que puede devolver 3 hojas: Pedido de venta de subscripción, Pedido de venta con errores y Suscripciones sin ventas\n'
+                '\n'
+                'Pedido de venta de subscripción: esta hoja va a mostrar todos los pedidos de venta que la fecha de confrimación este\n'
+                'dentro de los rangos de fechas ingresados y tenga una subscripción relacionada, si la base imponible del pedido\n'
+                'de venta es diferente al precio recurrente de la subscripción esta se pinta de amarillo.\n'
+                '\n'
+                'Pedido de venta con errores: esta hoja va a mostrar todos los pedidos de venta que la fecha de confrimación este\n'
+                'dentro de los rangos de fechas ingresados, sean de tipo subscripcion y no se encuentren una subscripción relacionada.\n'
+                '\n'
+                'Suscripciones sin ventas: esta hoja va a mostrar todas los suscripciones que deberian tener uno o varios pedido de venta creado\n'
+                'dentro de los rangos de fechas ingresados y se encontraron menos, en el campo Detalle del error muestra cuantos\n'
+                'pedidos de ventas deberian haber y cuantos se encontraron.\n',
+        string='Descripción:', readonly=True)
+    file = fields.Binary(string="Report")
+
+    @staticmethod
+    def get_next_interval(template, interval_date):
+        if template.recurring_rule_type == 'daily':
+            interval_date += relativedelta(days=template.recurring_interval)
+        elif template.recurring_rule_type == 'weekly':
+            interval_date += relativedelta(weeks=template.recurring_interval)
+        elif template.recurring_rule_type == 'monthly':
+            interval_date += relativedelta(months=template.recurring_interval)
+        elif template.recurring_rule_type == 'yearly':
+            interval_date += relativedelta(years=template.recurring_interval)
+        else:
+            raise UserError(_('Dont contemplate %s recurring interval type, communicate with TI') % template.recurring_rule_type)
+
+        return interval_date
+
+    def get_subscription_without_sales(self):
+        subscription_without_sales = []
+        in_process_stage_id = self.env['sale.subscription.stage'].search([('category', '=', 'progress')], limit=1).id
+        subscriptions = self.env['sale.subscription'].search([('stage_id', '=', in_process_stage_id), ('date_start', '<=', self.stop), '|', ('date', '>=', self.start), ('date', '=', False)])
+        for sub in subscriptions:
+            so_count = 0
+            interval_date = sub.date_start
+            while interval_date <= self.stop:
+                if interval_date >= self.start:
+                    so_count += 1
+                interval_date = self.get_next_interval(sub.template_id, interval_date)
+            if so_count:
+                so_list = []
+                sol = self.env['sale.order.line']
+                sol_ids = sol.search([('subscription_id', '=', sub.id)])
+                for sol in sol_ids:
+                    if sol.order_id.id not in so_list:
+                        so_list.append(sol.order_id.id)
+                if so_count > len(so_list):
+                    subscription_without_sales.append([sub.name, sub.recurring_total, sub.template_id.name,
+                                                       ('Should have %d but only found %d Sale Orders' % (so_count, len(so_list)))])
+
+        return subscription_without_sales
+
+    @staticmethod
+    def sale_order_subscription_excel(sales_info_list, sales_error_list, subscription_without_sales):
+        wb = Workbook(encoding='utf-8')
+        row = 0
+        title = easyxf('font: name Calibri, bold True; alignment: horizontal left')
+        lines = easyxf('font: name Calibri; alignment: horizontal left')
+        lines_number = easyxf('font: name Calibri; alignment: horizontal right',
+                              num_format_str='#,##0.000;-#,##0.000;')
+        lines_error = easyxf('font: name Calibri; alignment: horizontal left; pattern: pattern solid,'
+                                      ' fore_colour light_yellow;')
+        lines_number_error = easyxf('font: name Calibri; alignment: horizontal right; pattern: pattern solid,'
+                                      ' fore_colour light_yellow;', num_format_str='#,##0.00;-#,##0.00;')
+        lines_number_total = easyxf('font: name Calibri, bold True; alignment: horizontal right',
+                              num_format_str='#,##0.000;-#,##0.000;')
+        if sales_info_list:
+            ws = wb.add_sheet('Pedido de venta de subscripción', cell_overwrite_ok=True)
+            ws.write(row, 0, "Venta", title)
+            ws.write(row, 1, "Cliente", title)
+            ws.write(row, 2, "Subscripción", title)
+            ws.write(row, 3, "Tipo de subscripción", title)
+            ws.write(row, 4, "Fecha de confirmación", title)
+            ws.write(row, 5, "Precio recurrente", title)
+            ws.write(row, 6, "Total pedido de venta", title)
+            for info in sales_info_list:
+                row += 1
+                if info[5] == info[6]:
+                    ws.write(row, 0, info[0], lines)
+                    ws.write(row, 1, info[1], lines)
+                    ws.write(row, 2, info[2], lines)
+                    ws.write(row, 3, info[3], lines)
+                    ws.write(row, 4, info[4], lines)
+                    ws.write(row, 5, info[5], lines_number)
+                    ws.write(row, 6, info[6], lines_number)
+                else:
+                    ws.write(row, 0, info[0], lines_error)
+                    ws.write(row, 1, info[1], lines_error)
+                    ws.write(row, 2, info[2], lines_error)
+                    ws.write(row, 3, info[3], lines_error)
+                    ws.write(row, 4, info[4], lines_error)
+                    ws.write(row, 5, info[5], lines_number_error)
+                    ws.write(row, 6, info[6], lines_number_error)
+            row += 1
+            ws.write(row, 5, Formula("SUM(F1 :F%s)" % row), lines_number_total)
+            ws.write(row, 6, Formula("SUM(G1 :G%s)" % row), lines_number_total)
+            ws.col(0).width += 725
+            ws.col(1).width += 725
+            ws.col(2).width += 725
+            ws.col(3).width += 725
+            ws.col(4).width += 725
+            ws.col(5).width += 725
+            ws.col(6).width += 725
+
+        if sales_error_list:
+            row = 0
+            ws = wb.add_sheet('Pedido de venta con errores', cell_overwrite_ok=True)
+            ws.write(row, 0, "Venta", title)
+            ws.write(row, 1, "Cliente", title)
+            ws.write(row, 2, "Fecha de confirmación", title)
+            ws.write(row, 3, "Total pedido de venta", title)
+            for info_error in sales_error_list:
+                row += 1
+                ws.write(row, 0, info_error[0], lines)
+                ws.write(row, 1, info_error[1], lines)
+                ws.write(row, 2, info_error[2], lines)
+                ws.write(row, 3, info_error[3], lines_number)
+            ws.col(0).width += 725
+            ws.col(1).width += 725
+            ws.col(2).width += 725
+            ws.col(3).width += 725
+
+        if subscription_without_sales:
+            row = 0
+            ws = wb.add_sheet('Suscripciones sin ventas', cell_overwrite_ok=True)
+            ws.write(row, 0, "Suscripción", title)
+            ws.write(row, 1, "Precio Recurrente", title)
+            ws.write(row, 2, "Tipo de Suscripción", title)
+            ws.write(row, 3, "Detalle del error", title)
+            for subs in subscription_without_sales:
+                row += 1
+                ws.write(row, 0, subs[0], lines)
+                ws.write(row, 1, subs[1], lines_number)
+                ws.write(row, 2, subs[2], lines)
+                ws.write(row, 3, subs[3], lines)
+            ws.col(0).width += 1500
+            ws.col(1).width += 2000
+            ws.col(2).width += 3000
+            ws.col(3).width += 8000
+
+        return wb
+
+    @api.multi
+    def sale_order_subscription_report(self):
+        subscription_type_id = self.env['sale.order.type'].search([('name', '=', u'Suscripción')], limit=1)
+        if not subscription_type_id:
+            raise UserError(_('Dont found the sale order type Subscription'))
+        sale_order_with_subscription_ids = self.env['sale.order'].search([('sale_order_type_id', '=', subscription_type_id.id), ('date_order', '>=', self.start), ('date_order', '<=', self.stop), ('state', '!=', 'cancel')])
+        if not sale_order_with_subscription_ids:
+            raise UserError(_('Dont found any Sale Order from subscription in that range of dates'))
+        sales_info_list = []
+        sales_error_list = []
+        for sale in sale_order_with_subscription_ids:
+            info = []
+            subscription = self.env['sale.subscription'].search([('code', '=', sale.origin)])
+            if not sale.origin or not subscription:
+                info.append(sale.name)
+                info.append(sale.partner_id.name)
+                info.append(sale.date_order.strftime("%d/%m/%Y"))
+                info.append(sale.amount_untaxed)
+                sales_error_list.append(info)
+            else:
+                if len(subscription) > 1:
+                    raise UserError(_('Found more than one subscription with name %s' % sale.origin))
+                info.append(sale.name)
+                info.append(sale.partner_id.name)
+                info.append(subscription.code)
+                info.append(subscription.template_id.name)
+                info.append(sale.date_order.strftime("%d/%m/%Y"))
+                info.append(subscription.recurring_total)
+                info.append(sale.amount_untaxed)
+                sales_info_list.append(info)
+        subscription_without_sales = self.get_subscription_without_sales()
+        excel = self.sale_order_subscription_excel(sales_info_list, sales_error_list, subscription_without_sales)
+
+        fp = BytesIO()
+        excel.save(fp)
+        fp.seek(0)
+        data = fp.read()
+        fp.close()
+        file_name = 'Pedido de venta de subscripcion.xls'
+        data_to_save = base64.encodebytes(data)
+        wiz_id = self.env['descargar.hojas'].create({'archivo_nombre': file_name, 'archivo_contenido': data_to_save})
+        return {
+            'name': "Descargar Archivo",
+            'type': 'ir.actions.act_window',
+            'res_model': 'descargar.hojas',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': wiz_id.id,
+            'views': [(False, 'form')],
+            'target': 'new',
+        }
