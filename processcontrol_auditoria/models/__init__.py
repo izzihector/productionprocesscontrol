7d6e91ddb393ca89a2b8f916ec482aaeb0becf07
Agustin <aforbes@proyectasoft.com>
2021-07-14 09:38:28 -0300

    Pasar al módulo de auditoría en la v12 el reporte cuenta analítica parte horas

--- processcontrol_auditoria/models/__init__.py
+++ processcontrol_auditoria/models/__init__.py
@@ -2,3 +2,4 @@ from . import sale_order_subscription_report
 from . import sale_order_cost_in_zero
 from . import project_without_sale_order_item
 from . import descargar_hojas
+from . import time_sheet_analytic_account
