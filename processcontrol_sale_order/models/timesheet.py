29e6f3df1a0e035918e8f660863bc2e63aefab96
Agustin <aforbes@proyectasoft.com>
2021-09-03 07:52:26 -0300

    Hacer que el campo de Coste en la linea del pedido de venta sea readonly cuando el state = done  y Solucionar error al eliminar parte de horas

--- processcontrol_sale_order/models/timesheet.py
+++ processcontrol_sale_order/models/timesheet.py
@@ -8,8 +8,9 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
     def unlink(self):
-        if self.is_timesheet:
-            if not self.env.user.has_group('processcontrol_sale_order.group_can_delete_timesheet'):
-                raise ValidationError(_('Error. No tienes el permiso para poder eliminar una parte de hora'))
+        for rec in self:
+            if rec.is_timesheet:
+                if not rec.env.user.has_group('processcontrol_sale_order.group_can_delete_timesheet'):
+                    raise ValidationError(_('Error. No tienes el permiso para poder eliminar una parte de hora'))
         return super(AccountAnalyticLine, self).unlink()
 
