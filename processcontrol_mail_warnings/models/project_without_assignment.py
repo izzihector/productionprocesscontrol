911954a9de9bf87b2a71156f949afdb2460446a8
carolinafernandez-processcontrol <84458198+carolinafernandez-processcontrol@users.noreply.github.com>
2021-09-07 10:55:09 -0300

    Update project_without_assignment.py

--- processcontrol_mail_warnings/models/project_without_assignment.py
+++ processcontrol_mail_warnings/models/project_without_assignment.py
@@ -31,7 +31,7 @@ class Project(models.Model):
     def cron_project_without_assignment(self):
         project_project_obj = self.env['project.project']
         less_seven_days = date.today() - timedelta(days=7)
-        project_without_assignment_less_than_seven_days_ids = project_project_obj.search([('create_date', '<=', seven_days),('user_id','=',False)])
+        project_without_assignment_less_than_seven_days_ids = project_project_obj.search([('create_date', '>=', less_seven_days),('user_id','=',False)])
         if project_without_assignment_less_than_seven_days_ids:
             project_without_assignment_more_than_seven_days_ids = project_project_obj.search([('ids', 'not in', (project_without_assignment_less_than_seven_days_ids.ids)),('user_id','=',False)])
         else:
