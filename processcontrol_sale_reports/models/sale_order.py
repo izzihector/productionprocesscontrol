b12b61e4728c954a0d0b4635b18640ec40d02b02
David Ramia <david.ramia@processcontrol.es>
2021-09-09 10:25:47 +0200

    Fix division entre 0

--- processcontrol_sale_reports/models/sale_order.py
+++ processcontrol_sale_reports/models/sale_order.py
@@ -10,11 +10,11 @@ class SaleOrderLine(models.Model):
     @api.depends('price_subtotal', 'qty_delivered', 'purchase_price')
     def _compute_margin_delivered(self):
         for line in self:
-            price = line.price_subtotal / line.product_uom_qty
+            price = (line.price_subtotal / line.product_uom_qty) if line.product_uom_qty else 0
             line.margin_delivered = price * line.qty_delivered - (line.purchase_price * line.qty_delivered)
 
     @api.depends('price_subtotal', 'qty_invoiced', 'purchase_price')
     def _compute_margin_invoiced(self):
         for line in self:
-            price = line.price_subtotal / line.product_uom_qty
+            price = (line.price_subtotal / line.product_uom_qty) if line.product_uom_qty else 0
             line.margin_invoiced = price * line.qty_invoiced - (line.purchase_price * line.qty_invoiced)
