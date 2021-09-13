b5627d201d61634ea60b20dbb598682e4389641f
carolinafernandez-processcontrol <84458198+carolinafernandez-processcontrol@users.noreply.github.com>
2021-09-13 09:01:48 -0300

    Update timesheet.py

--- processcontrol_sale_order/models/timesheet.py
+++ processcontrol_sale_order/models/timesheet.py
@@ -9,8 +9,35 @@ class AccountAnalyticLine(models.Model):
 
     def unlink(self):
         for rec in self:
+            user = rec.env.user
+            employee_id = self.env['hr.employee'].search([('user_id', '=', user)], limit=1)
             if rec.is_timesheet:
-                if not rec.env.user.has_group('processcontrol_sale_order.group_can_delete_timesheet'):
-                    raise ValidationError(_('Error. No tienes el permiso para poder eliminar una parte de hora'))
+                if rec.employee_id.id != employee_id.id and not rec.env.user.has_group('processcontrol_sale_order.group_can_delete_timesheet'):
+                    raise ValidationError(_('Error. No se puede eliminar una parte de hora de otro empleadoi'))
         return super(AccountAnalyticLine, self).unlink()
 
+
+    @api.model
+    def create(self, values):
+        user = self._uid
+        employee_id = self.env['hr.employee'].search([('user_id','=',user)],limit=1)
+        if employee_id:
+            if 'is_timesheet' in values and values['is_timesheet']:
+                if 'employee_id' in values and values['employee_id'] != False:
+                    if values['employee_id'] != employee_id.id and not rec.env.user.has_group('processcontrol_sale_order.group_can_delete_timesheet'):
+                        raise ValidationError(_('Error. No se puede crear una parte de hora para otro empleado'))
+        return super(AccountAnalyticLine, self).create(values)
+
+    def write(self, vals):
+        for rec in self:
+            user = rec._uid
+            employee_id = self.env['hr.employee'].search([('user_id', '=', user)], limit=1)
+            if rec.is_timesheet:
+                if 'employee_id' in values and values['employee_id'] != False:
+                    employee_to_modify_id = values['employee_id']
+                else:
+                    employee_to_modify_id=rec.employee_id.id
+                if employee_to_modify_id != employee_id.id and not rec.env.user.has_group(
+                        'processcontrol_sale_order.group_can_delete_timesheet'):
+                    raise ValidationError(_('Error. No se puede modificar una parte de hora de otro empleado'))
+        return super(AccountAnalyticLine, self).write(values)
