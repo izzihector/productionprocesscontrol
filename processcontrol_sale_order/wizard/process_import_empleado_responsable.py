788e4cfb39c098e7a9b617c45f2dbd1e4f259332
carolinafernandez-processcontrol <84458198+carolinafernandez-processcontrol@users.noreply.github.com>
2021-09-22 17:07:35 -0300

    Update process_import_empleado_responsable.py

--- processcontrol_sale_order/wizard/process_import_empleado_responsable.py
+++ processcontrol_sale_order/wizard/process_import_empleado_responsable.py
@@ -28,10 +28,11 @@ class PCImportEmpleadoResponsable(models.TransientModel):
         else add the SaleOrderName in dont_found
         """
         count_so=float()
+        updated=str()
         self.dont_found = False
         if not self.archive:
             sale_orders = self.env['sale.order'].search([('user_id','!=',False)])
-            teams= self.env['crm.team'].search([]) 
+            teams= self.env['crm.team'].search([])
             for sale in sale_orders:
                 if sale.user_id not in sale.team_id.member_ids:
                     for team in teams:
@@ -39,7 +40,8 @@ class PCImportEmpleadoResponsable(models.TransientModel):
                             if (sale.user_id.id == 407 and sale.create_date <= '2021-08-31') or sale.user_id.id != 407:
                                 sale.team_id = team.id
                                 count_so+=1
-            self.updated += 'Se actualizaron %s pedidos de venta' % count_so + '\n'
+            if count_so >0:
+                updated += 'Se actualizaron %s pedidos de venta' % count_so + '\n'
             count_so = float()
             opportunities = self.env['crm.lead'].search([('type','=','opportunity'),('user_id', '!=', False)])
             for oportunity in opportunities:
@@ -49,7 +51,8 @@ class PCImportEmpleadoResponsable(models.TransientModel):
                             if (oportunity.user_id.id == 407 and oportunity.create_date <= '2021-08-31') or oportunity.user_id.id != 407:
                                 oportunity.team_id = team.id
                                 count_so+=1
-            self.updated += 'Se actualizaron %s oportunidades' % count_so + '\n'
+            if count_so >0:
+                updated += 'Se actualizaron %s oportunidades' % count_so + '\n'
             suscriptions = self.env['sale.subscription'].search([('user_id', '!=', False)])
             count_so = float()
             for suscription in suscriptions:
@@ -59,7 +62,11 @@ class PCImportEmpleadoResponsable(models.TransientModel):
                             if (suscription.user_id.id == 407 and suscription.create_date <= '2021-08-31') or suscription.user_id.id != 407:
                                     suscription.team_id = team.id
                                     count_so+=1
-            self.updated += 'Se actualizaron %s suscripciones' % count_so + '\n'
+            if count_so >0:
+                updated += 'Se actualizaron %s suscripciones' % count_so + '\n'
+
+            if updated:
+                self.updated = updated
 
             #     suscription = self.env['sale.subscription'].search([('code','=',sale.origin)],limit=1)
             #     if suscription:
